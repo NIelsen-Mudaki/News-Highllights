34e67d78994405c98a53c355c68be2e98410de1a
Nielsen-Mudaki <jumbanielsen@gmail.com>
2022-02-15 20:35:23 +0300

    add category template

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from . import main
-from ..requests import get_source, news_source
+from ..requests import get_source, news_source, get_headlines,get_category
 
 # Views
 @main.route('/')
@@ -10,8 +10,9 @@ def index():
     '''
 
     source = get_source()
+    headlines = get_headlines()
 
-    return render_template('index.html', sources=source)
+    return render_template('index.html', sources=source, headlines=headlines)
 
 
 @main.route('/article/<id>')
@@ -23,3 +24,13 @@ def article(id):
     
     news = news_source(id)
     return render_template('news.html',news = news,id=id )
+
+@main.route('/categories/<cat_name>')
+def category(cat_name):
+    '''
+    function to return the categories.html page and its content
+    '''
+    category = get_category(cat_name)
+    title = f'{cat_name}'
+
+    return render_template('categories.html',title = title,category = category)
--- app/models.py
+++ app/models.py
@@ -36,3 +36,15 @@ class Category:
         self.url = url
         self.image = image
         self.title = title
+
+class Headlines:
+    '''
+    Class that instantiates objects of the headlines categories objects of the news sources
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
