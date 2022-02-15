68954d48795b70e97b221b755b54e384112f6141
Nielsen-Mudaki <jumbanielsen@gmail.com>
2022-02-15 15:52:59 +0300

    display news sources

--- app/models.py
+++ app/models.py
@@ -17,9 +17,21 @@ class NewsSource:
         Class that defines a source object.
     '''
 
-    def __init__(self,id,name,description,url,category,language):
+    def __init__(self,id,name,description,url):
         self.id = id
         self.name = name
         self.description = description
         self.url = url
         
+
+class Category:
+    '''
+    Class that instantiates objects of the news categories objects of the news sources
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export MOVIE_API_KEY=<Your api key>
-export SECRET_KEY=<Your secret key>
+export NEWS_API_KEY='8bfbc02859464b878919b033269f6b35'
+
 
 python3.6 manage.py server
